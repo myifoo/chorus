bbb0280cd9009e9480bf7d930832d2401d7ce829
myifoo <myima@sina.com>
2019-04-11 17:57:41 +0800

    update some files

--- src/main/java/com/platform/chorus/ChorusServerApplication.java
+++ src/main/java/com/platform/chorus/ChorusServerApplication.java
@@ -1,13 +1,119 @@
 package com.platform.chorus;
 
+import com.platform.chorus.database.utils.PgsqlSchemaTool;
+import org.apache.commons.cli.*;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+import org.springframework.lang.Nullable;
 
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ *
+ *  <p> Class Description </p>
+ *
+ *      Server Author A.T
+ *
+ *  @version 1.0 2018/11/06 A.T
+ *
+ */
 @SpringBootApplication
-public class ChorusServerApplication {
+public class ChorusServerApplication implements ApplicationContextAware
+{
+	//    private static Logger logger = LoggerFactory.getLogger(Server.class);
+	private static ApplicationContext context;
+	private static SpringApplication application;
+	private static Environment environment;
+	private static CommandLine cmd;
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChorusServerApplication.class, args);
+	static private void printHelpMessage() {
+		StringBuilder usage = new StringBuilder();
+		String spaces = "        ";
+		usage.append("\n")
+				.append("Usage   : java -jar ailysis-master-1.0-SNAPSHOT.jar [options] [args...]\n")
+				.append("Example : java -jar ailysis-master-1.0-SNAPSHOT.jar -d init -u ailysis -p ailysis123 -ip localhost -port 5432 \n")
+				.append("\n\n")
+				.append(spaces).append("-h -help       :  Print this usage information\n")
+				.append(spaces).append("-m -mode       :  Server mode, such as [ web, test, all ]\n")
+				.append(spaces).append("-d -db         :  database command line tools, such as [ init, clear, refresh, migrate]\n")
+				.append(spaces).append("-u             :  database username, required when execute db command\n")
+				.append(spaces).append("-p             :  database password, required when execute db command\n")
+				.append(spaces).append("-ip            :  database ip, required when execute db command\n")
+				.append(spaces).append("-port          :  database port, required when execute db command\n");
+
+		System.out.println(usage.toString());
+		System.exit(0);
 	}
 
+	public static void main( String[] args )
+	{
+		CommandLineParser parser = new DefaultParser();
+		Options options = new Options();
+
+		options.addOption("h", "help", false, "Print usage information");
+		options.addOption("m", "mode", true, "Server mode: [ model, test, all ]");
+		options.addOption("d", "db", true, "database command line tools: [ init, clear, refresh, migrate]");
+		options.addOption("u", "username", true, "database username");
+		options.addOption("p", "password", true, "database password");
+		options.addOption("ip", true, "database ip");
+		options.addOption("port", true, "database port");
+
+		try {
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			printHelpMessage();
+			exit(1);
+		}
+		if (cmd.hasOption("h")){
+			printHelpMessage();
+			exit(0);
+		}
+
+		if (cmd.hasOption("d")){
+			PgsqlSchemaTool.execute(cmd);
+			exit(0);
+		}
+
+		application = new SpringApplication(ChorusServerApplication.class);
+		application.setBannerMode(Banner.Mode.OFF);
+		application.run(args);
+	}
+
+
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		context = applicationContext;
+		environment = applicationContext.getEnvironment();
+	}
+
+	static public void exit(int code) {
+		System.exit(code);
+	}
+
+	public static boolean isTestMode() {
+		String mode = cmd.getOptionValue("m");
+
+		if (isWebOnlyMode() || Objects.nonNull(mode) && "test".equals(mode)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isWebOnlyMode() {
+		String mode = cmd.getOptionValue("m");
+
+		if (Objects.nonNull(mode) && "web".equals(mode)) {
+			return true;
+		}
+		return false;
+	}
 }
---
+++ src/main/java/com/platform/chorus/database/utils/PgsqlSchemaTool.java
@@ -0,0 +1,93 @@
+package com.platform.chorus.database.utils;
+
+import org.apache.commons.cli.CommandLine;
+import org.jooq.DSLContext;
+import org.jooq.DataType;
+import org.jooq.SQLDialect;
+import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DSL;
+import org.jooq.impl.SQLDataType;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Create by A.T on 2019/4/11
+ */
+public class PgsqlSchemaTool {
+    private String ip;
+    private String port;
+    private String user;
+    private String password;
+
+    PgsqlSchemaTool(String ip, String port, String user, String password) {
+        this.ip = ip;
+        this.port = port;
+        this.user = user;
+        this.password = password;
+    }
+
+    static public void execute(CommandLine cmd) {
+        PgsqlSchemaTool instance = new PgsqlSchemaTool(cmd.getOptionValue("ip"), cmd.getOptionValue("port"),
+                cmd.getOptionValue("u"), cmd.getOptionValue("p"));
+
+        String action = cmd.getOptionValue("d");
+
+        if (action.equals("init")) {
+            instance.createDatabase();
+        }
+    }
+
+    private boolean testSchema(String url, String user, String password){
+        try {
+            Connection connection = DriverManager.getConnection(url, user, password);
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    private void createDatabase() {
+        String name = "chorusdb"; //todo
+
+        try (DSLContext dsl = DSL.using(DriverManager.getConnection(String.format("jdbc:postgresql://%s:%s/postgres", ip, port), user, password), SQLDialect.POSTGRES)) {
+            System.out.println("Connect to database successfully");
+
+            try {
+                System.out.println("firstly, drop ailysisdb if exist ...");
+                dsl.execute("drop database "+name);
+            } catch (DataAccessException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.println("now, start to create db ...");
+            dsl.execute(String.format("CREATE DATABASE %s OWNER %s;", name, user)); // todo
+            System.out.println("Create DB done!");
+        } catch (Exception e) {
+            System.out.println(String.format("create database failed : %s", e.getMessage()));
+        }
+
+        try (DSLContext dsl = DSL.using(DriverManager.getConnection(String.format("jdbc:postgresql://%s:%s/%s", ip, port, name), user, password), SQLDialect.POSTGRES)) {
+            System.out.println("Start to create tables ...");
+            Resource resource = new PathMatchingResourcePatternResolver().getResource("classpath:schema/chorus_tables.sql");
+            BufferedReader reader = new BufferedReader(new FileReader(resource.getFile()));//new File("src/main/resources/schema/ailysis.sql")
+            String sql = reader.lines()
+                    .filter(l -> !(l.isEmpty() || l.toUpperCase().contains("DROP")))
+                    .map(l-> l.split("--")[0])
+                    .reduce("", (a, b)-> a+b);
+
+            String[] sqls = sql.split(";");
+            dsl.batch(sqls).execute();
+
+            System.out.println("Create tables done!");
+        } catch (Exception e) {
+            System.out.println(String.format("create tables failed : %s", e.getClass().getSimpleName()));
+        }
+    }
+}
